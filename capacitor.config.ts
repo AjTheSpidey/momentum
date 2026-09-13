215248aafb14d211045177fd9fb26adbe45785cf
Aadi Jai Gupta <168258894+AjTheSpidey@users.noreply.github.com>
2026-09-13 18:22:48 +0800

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.momentum.app',
+  appName: 'Momentum',
+  webDir: 'dist/public',
+  bundledWebRuntime: false,
+};
+
+export default config;
